b93739f03a9696425989700bcce315c4dd88ad54
Nicholas <49096411+Nickz1n@users.noreply.github.com>
2025-07-07 18:42:27 -0300

    update PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,41 @@
+import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+export default defineConfig({
+  plugins: [
+    sveltekit(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Nicholas Nascimento | Portfólio',
+        short_name: 'Portfólio',
+        description: 'Meu portfólio como desenvolvedor de software',
+        theme_color: '#121212',
+        background_color: '#121212',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+});
